f72bb7d9774ca78907915d5bf639ae67bcc1b648
NeighborTone <126633470+t-sato-tasto@users.noreply.github.com>
2023-11-08 03:27:55 +0900

    init

---
+++ convert_to_utf-8.py
@@ -0,0 +1,43 @@
+﻿import os
+import chardet # pip install chardetが必要
+import codecs
+
+def convert_to_utf8_bom(file_path):
+    # ファイルをバイナリモードで読み込み、文字コードを検出
+    with open(file_path, 'rb') as f:
+        raw_data = f.read()
+        encoding = chardet.detect(raw_data)['encoding']
+
+    # 文字コードが既にUTF-8 BOMの場合はスキップ
+    if encoding != 'utf-8':
+        # ファイルの内容をデコードし、UTF-8 BOM付きでエンコード
+        try:
+            decoded_data = raw_data.decode(encoding)
+            encoded_data = codecs.BOM_UTF8 + decoded_data.encode('utf-8')
+            
+            # 変換したデータを同じファイルに書き込む
+            with open(file_path, 'wb') as f:
+                f.write(encoded_data)
+        except UnicodeDecodeError:
+            print(f"Unicode decode error in file {file_path}, skipping...")
+        except UnicodeEncodeError:
+            print(f"Unicode encode error in file {file_path}, skipping...")
+        except TypeError:
+            print(f"Type error in file {file_path}, skipping...")
+        except Exception as e:
+            print(f"Error converting file {file_path}: {e}")
+
+def main():
+    # スクリプトがあるディレクトリを取得
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # 再帰的にフォルダ内のファイルをリストアップ
+    for root, dirs, files in os.walk(root_dir):
+        for file in files:
+            # ファイルのフルパスを取得
+            file_path = os.path.join(root, file)
+            # UTF-8に変換
+            convert_to_utf8_bom(file_path)
+
+if __name__ == '__main__':
+    main()
